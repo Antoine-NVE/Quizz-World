67a374724c4c097d4526e817a6cc81f91a33d91c
Antoine <antoinenavette@gmail.com>
2023-09-25 17:24:18 +0200

    Boutons fonctionnels

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const container = document.getElementById("container");
+const boutons = document.getElementsByClassName("boutons");
 
 class Quizz {
   constructor(titre, nom, extension) {
@@ -12,11 +13,11 @@ class Quizz {
     <div class="cartes">
       <h2>${this.titre}</h2>
       <img src="img/${this.nom}.${this.extension}" alt="${this.nom}">
-      <input id="${this.nom}Debutant" type="radio" name="${this.nom}">
+      <input class="boutons" id="${this.nom}Debutant" type="radio" name="${this.nom}">
       <label for="${this.nom}Debutant">Débutant</label>
-      <input id="${this.nom}Confirme" type="radio" name="${this.nom}">
+      <input class="boutons" id="${this.nom}Confirme" type="radio" name="${this.nom}">
       <label for="${this.nom}Confirme">Confirmé</label>
-      <input id="${this.nom}Expert" type="radio" name="${this.nom}">
+      <input class="boutons" id="${this.nom}Expert" type="radio" name="${this.nom}">
       <label for="${this.nom}Expert">Expert</label>
     </div>
     `;
@@ -32,7 +33,7 @@ const microsoft = new Quizz("Microsoft", "microsoft", "jpg");
 const php = new Quizz("PHP", "php", "jpg");
 const internet = new Quizz("Méandres d'Internet", "internet", "jpg");
 
-container.innerHTML += web.carte;
+container.innerHTML = web.carte;
 container.innerHTML += javascript.carte;
 container.innerHTML += dates20.carte;
 container.innerHTML += nintendo.carte;
@@ -40,3 +41,32 @@ container.innerHTML += nombres.carte;
 container.innerHTML += microsoft.carte;
 container.innerHTML += php.carte;
 container.innerHTML += internet.carte;
+
+for (i = 0; i < boutons.length; i++) {
+  const checked = document.getElementById(boutons[i].id);
+  checked.addEventListener("click", () => {
+    console.log(checked);
+
+    if (checked.id.includes("Debutant")) {
+      niveau = "débutant";
+    } else if (checked.id.includes("Confirme")) {
+      niveau = "confirmé";
+    } else {
+      niveau = "expert";
+    }
+
+    fetch(`./json/quizz${checked.name}.json`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.quizz);
+        prenom = window.prompt("Veuillez saisir votre prénom");
+
+        container.style.display = "block";
+        container.innerHTML = `
+        <h2>${checked.parentElement.children[0].textContent} - Niveau ${niveau}</h2>
+        <h2><span>${prenom}</span>, vous allez pouvoir démarrer ce Quizz</h2>
+        <img src="img/${checked.name}.png" alt="${checked.name}">
+        `;
+      });
+  });
+}
